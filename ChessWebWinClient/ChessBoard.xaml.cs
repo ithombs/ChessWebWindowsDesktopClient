9bccd445b7ccc991324e37757c166ac7a8a8f6f9
ithombs <thombsii@gmail.com>
2015-08-10 08:13:25 -0400

    Rebuilt the chess board control. Dragging and dropping pieces works now! Yay!

--- ChessWebWinClient/ChessBoard.xaml.cs
+++ ChessWebWinClient/ChessBoard.xaml.cs
@@ -19,13 +19,15 @@ namespace ChessWebWinClient
     /// </summary>
     public partial class ChessBoard : UserControl
     {
-
+        List<HitTestResult> hitResultsList;
 
         public ChessBoard()
         {
             InitializeComponent();
-
+            hitResultsList = new List<HitTestResult>();
+            
         }
+        
 
         private void ThumbChessPiece_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
         {
@@ -35,6 +37,34 @@ namespace ChessWebWinClient
 
             Canvas.SetLeft(thumb, Canvas.GetLeft(thumb) + e.HorizontalChange);
             Canvas.SetTop(thumb, Canvas.GetTop(thumb) + e.VerticalChange);
+        }
+
+        private void testPiece1_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            hitResultsList.Clear();
+
+            ThumbChessPiece piece = (ThumbChessPiece)sender;
+            Console.WriteLine("Top:{0} | Left: {1}", Canvas.GetTop(piece), Canvas.GetLeft(piece));
+
+            VisualTreeHelper.HitTest(chessCanvas, null, 
+                new HitTestResultCallback(MyHitTestResults), 
+                new PointHitTestParameters(e.GetPosition(chessCanvas)));
+
+            Rectangle r = (Rectangle)hitResultsList.Last().VisualHit;
+            Console.WriteLine(r.Name);
+            Console.WriteLine("Top:{0} | Left: {1}", Canvas.GetTop(r), Canvas.GetLeft(r));
+            
+            //Snap the piece to the square it was dropped in (where the mouse was released)
+            Canvas.SetTop(piece, Canvas.GetTop(r));
+            Canvas.SetLeft(piece, Canvas.GetLeft(r));
+            
+        }
+
+        public HitTestResultBehavior MyHitTestResults(HitTestResult result)
+        {
+            hitResultsList.Add(result);
+
+            return HitTestResultBehavior.Continue;
         }
     }
 }

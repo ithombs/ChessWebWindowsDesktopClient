3daf5aa2f0f265f7e90f80b1ba222f9e221814fb
ithombs <thombsii@gmail.com>
2017-02-03 11:31:00 -0500

    Library additions and minor UI changes

--- ChessWebWinClient/MainWindow.xaml.cs
+++ ChessWebWinClient/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BCrypt.Net;
 
 namespace ChessWebWinClient
 {
@@ -47,6 +48,7 @@ namespace ChessWebWinClient
             side = lblColor;
             info = lblInfo;
             enterQueue = btnEnterQueue;
+
         }
 
         private void initTileDictionary()
